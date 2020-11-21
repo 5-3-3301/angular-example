9d03c371831d6838e31bbafc587e0d2b9b3d09e2
xmshen <xmshen@thoughtworks.com>
2020-11-21 18:33:06 +0800

    [sxm][form]feat: add single responsive form group.

--- src/app/module/form/components/responsive-create/responsive-create.component.ts
+++ src/app/module/form/components/responsive-create/responsive-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-responsive-create',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./responsive-create.component.less']
 })
 export class ResponsiveCreateComponent implements OnInit {
+  studentForm: FormGroup;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.studentForm = new FormGroup({
+      name: new FormControl(''),
+      age: new FormControl('')
+    });
+  }
+
+  onSubmitStudentInfo(): void {
+    alert('学生信息： ' + JSON.stringify(this.studentForm.value));
+  }
 }
